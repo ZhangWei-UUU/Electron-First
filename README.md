03c7c4bd7d0e1e52f9eec3c12eda12e25f584b59
ZhangWei <350095093@qq.com>
2018-11-04 17:20:35 +0800

    add electron packager

--- README.md
+++ README.md
@@ -4,5 +4,27 @@
 
 ```
 npm init
-yarn add electron
+yarn add electron --dev
 ```
+
+## Head Mene
+
+Creating a menu array,then use Menu object from electron module.
+
+```js
+// create menu template
+const headMenu = Menu.buildFromTemplate(menuArray);
+// setting
+Menu.setApplicationMenu(headMenu);
+```
+
+## Packager
+
+```
+yarn add electron-packager --dev
+```
+
+```json
+    {"package-mac": "electron-packager . --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds"}
+```
+
--- index.js
+++ index.js
@@ -2,15 +2,51 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowerWindow } = electron;
+const { app, BrowserWindow,Menu } = electron;
 
-let mainWindow;
+const addItem = () =>{
+    let smallWindow = new BrowserWindow({width: 200, height: 600});
+    smallWindow.loadURL(url.format({
+        pathname:path.join(__dirname,"smallWindow.html"),
+        slashes:true,
+        protocol:"file:"
+    }));
+    smallWindow.on('close',()=>{
+        smallWindow = null
+    })
+}
+const MenuArray = [
+    {label:"Electron",submenu:[
+        {label:"新增",
+        click(){
+          addItem()
+        }
+    },
+        {label:"开发者工具",accelerator:process.platform === 'darwin'?"Command+I":"Ctrl+F12",
+        click(item,focusWindow){
+            focusWindow.toggleDevTools();
+        }
+    },
+        
+    ]},
+    {label:"操作",submenu:[
+        {label:"退出",
+        accelerator:process.platform === 'darwin'?"Command+Q":"Ctrl+Q",
+        click(){
+            app.quit();
+        }
+    }
+    ]},
 
-app.on('ready',()=>{
-    mainWindow = new BrowerWindow({});
+]
+app.on('ready',function(){
+    let mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL(url.format({
         pathname:path.join(__dirname,"mainWindow.html"),
         slashes:true,
         protocol:"file:"
     }));
+
+    const HeadMenu = Menu.buildFromTemplate(MenuArray);
+    Menu.setApplicationMenu(HeadMenu)
 })
